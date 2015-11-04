01278c9e4b99bdb7228381e17b50bc4693d82bbd
Sia <siakaramalegos@gmail.com>
2015-11-04 08:45:21 -0600

    Separated date and time variables

--- greeting/app/controllers/say_controller.rb
+++ greeting/app/controllers/say_controller.rb
@@ -1,6 +1,8 @@
 class SayController < ApplicationController
   def hello
-    @time = Time.now
+    right_now = Time.now
+    @current_time = right_now.stamp('12:00AM')
+    @current_date = right_now.stamp('Monday, February 1, 2015')
   end
 
   def goodbye
