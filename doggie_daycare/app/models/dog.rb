519d933fc14fc2fcbb0bd56d83e43e7e90b03d56
Sia <siakaramalegos@gmail.com>
2015-11-09 13:58:25 -0600

    Added dogs to owner view, added validation

--- doggie_daycare/app/models/dog.rb
+++ doggie_daycare/app/models/dog.rb
@@ -1,3 +1,13 @@
 class Dog < ActiveRecord::Base
   belongs_to :owner
+
+  validates :name, :breed, :age, :owner_id, presence: true
 end
+
+
+   # t.string   "name"
+   #  t.string   "breed"
+   #  t.integer  "age"
+   #  t.datetime "created_at", null: false
+   #  t.datetime "updated_at", null: false
+   #  t.integer  "owner_id"
--- doggie_daycare/app/models/owner.rb
+++ doggie_daycare/app/models/owner.rb
@@ -1,3 +1,8 @@
 class Owner < ActiveRecord::Base
   has_many :dogs
+
+  validates :name, :phone, presence: true
 end
+
+    # t.string   "name"
+    # t.string   "phone"
